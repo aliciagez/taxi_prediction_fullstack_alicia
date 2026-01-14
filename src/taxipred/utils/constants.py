c1d2d081fff7804e1f26b82c743a4addf4ae8a95
Alicia Gezelius <alicia.gezelius@gamail.com>
2026-01-14 15:35:38 +0100

    stated traning

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 DATA_PATH = Path(__file__).parents[1] / "data"
 TAXI_DATA =  DATA_PATH / "taxi_trip_pricing.csv"
-TAXI_REMOVED_NA = DATA_PATH / "removedna.csv"
+TAXI_REMOVED_NA = DATA_PATH / "removena.csv"
 TAXI_FILLED_NA = DATA_PATH / "fillna.csv"
