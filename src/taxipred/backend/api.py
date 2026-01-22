abed02821a7f166763836233134bbb97e4df77f7
Alicia Gezelius <alicia.gezelius@gamail.com>
2026-01-22 13:01:33 +0100

    impoed the class wiht literarl module

--- src/taxipred/backend/api.py
+++ src/taxipred/backend/api.py
@@ -3,31 +3,20 @@ import pandas as pd
 import joblib
 from taxipred.utils.constants import RANDOM_FOREST, MODEL_PATH
 from pydantic import BaseModel, Field
+from typing import Literal
 
 df = pd.read_csv(RANDOM_FOREST)
 app = FastAPI()
 router = APIRouter(prefix="/api/data")
 
 
-class optional_input1(str):
-  Morning ="Morning"
-  Afternoon ="Afternoon"
-  Evening = "Evening"
-
-
-class optional_input2(str):
-  Weekend ="Weekend"
-  Weekday ="Weekday"
-
-#used LLM for how to get a specif sting and how to make them into a default 
-
 class must_input(BaseModel):
     km_lengt: float = Field()
     passangers: int = Field(gt=1, lt=10)
-    time_of_day: optional_input1 = optional_input1.Morning
-    time_of_week: optional_input2 = optional_input2.Weekday
+    time_of_day: Literal["Moring", "Afternoon", "Evening"] = "Afternoon" # check if this i mean
+    time_of_week: Literal["Weekday", "Weekday"] = "Weekday"
 
-class pred_output(BaseExceptionGroup):
+class pred_output(BaseModel):
    pred_taxi_price: float
 
 @router.get("")
@@ -38,4 +27,9 @@ def get_data():
 @router.post("/taxi_pred")
 def random_forset_ml(priceload: must_input):
     forest_data = pd.DataFrame(priceload.model_dump(), index=[0])
-    reg = joblib.load(MODEL_PATH /  "")
+    reg = joblib.load(MODEL_PATH /  "taxi_price_random.joblib") 
+    pred_price = reg.predict(forest_data) 
+    print(pred_price)
+    return{random_forset_ml: float(pred_price[0])}
+
+app.include_router(router=router)
