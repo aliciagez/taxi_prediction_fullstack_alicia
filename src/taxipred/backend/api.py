3035918decff32cd0881de7b8858d29bdad3fc4f
Alicia Gezelius <alicia.gezelius@gamail.com>
2026-01-22 11:56:43 +0100

    added router, classes for basemdoel, started on prediction

--- src/taxipred/backend/api.py
+++ src/taxipred/backend/api.py
@@ -8,13 +8,34 @@ df = pd.read_csv(RANDOM_FOREST)
 app = FastAPI()
 router = APIRouter(prefix="/api/data")
 
-class info_input():
+
+class optional_input1(str):
+  Morning ="Morning"
+  Afternoon ="Afternoon"
+  Evening = "Evening"
+
+
+class optional_input2(str):
+  Weekend ="Weekend"
+  Weekday ="Weekday"
+
+#used LLM for how to get a specif sting and how to make them into a default 
+
+class must_input(BaseModel):
+    km_lengt: float = Field()
+    passangers: int = Field(gt=1, lt=10)
+    time_of_day: optional_input1 = optional_input1.Morning
+    time_of_week: optional_input2 = optional_input2.Weekday
+
+class pred_output(BaseExceptionGroup):
+   pred_taxi_price: float
 
 @router.get("")
 def get_data():
     return df.to_dict(orient="records")
 
 
-@router.post("")
-def random_forset_ml():
-    pass
+@router.post("/taxi_pred")
+def random_forset_ml(priceload: must_input):
+    forest_data = pd.DataFrame(priceload.model_dump(), index=[0])
+    reg = joblib.load(MODEL_PATH /  "")
